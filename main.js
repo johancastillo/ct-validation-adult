f0f1ad342eb7b97612f68d6126e7b850d3a3d4b7
Johan Castillo <jcjohan2707@gmail.com>
2021-01-27 16:07:49 -0400

    New validation

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ let dayEl = document.getElementById("day")
 let mounthEl = document.getElementById("mounth")
 let yearEl = document.getElementById("year")
 
-let day = dayEl.value
-let mounth = mounthEl.value
-let year = yearEl.value
+let day = parseInt(dayEl.value)
+let mounth = parseInt(mounthEl.value)
+let year = parseInt(yearEl.value)
+var anoActual = (new Date()).getFullYear();
+
+let fecha
+
 
 // Calcular edad
 function calculateAge(birthday) {
@@ -21,25 +25,44 @@ function calculateAge(birthday) {
 
 
 // OnChange
-dayEl.addEventListener('change', e => day = e.target.value)
-mounthEl.addEventListener('change', e => mounth = e.target.value)
-yearEl.addEventListener('change', e => year = e.target.value)
+dayEl.addEventListener('change', e => {
+    day = e.target.value
+    
+    // Validar si la feche es real
+    fecha = new Date(year, (mounth -1), day)
+})
+
+mounthEl.addEventListener('change', e => {
+    mounth = e.target.value
+    // Validar si la feche es real
+    fecha = new Date(year, (mounth -1), day)
+})
+
+yearEl.addEventListener('change', e => {
+    year = e.target.value
+    // Validar si la feche es real
+    fecha = new Date(year, (mounth -1), day)
+})
 
 
 // Event click
 btnSend.addEventListener('click', () => {
 
-    if (year && day) {
-        let age = calculateAge([day, mounth, year]);
-
-        if (age >= 18) {
-            sessionStorage.setItem('adult', true)
-            location.href = "https://compralotodo.com/categoria-producto/licores/"
-        } else {
-            message.textContent = "No puedes ver el contenido porque no eres mayor de edad."
+    if(!(year > anoActual) && !isNaN(year) && year != false && year == 0){
+        if (year && day) {
+            let age = calculateAge([day, mounth, year]);
+    
+            if (age >= 18) {
+                sessionStorage.setItem('adult', true)
+                location.href = "https://compralotodo.com/categoria-producto/licores/"
+            } else {
+                message.textContent = "No puedes ver el contenido porque no eres mayor de edad."
+            }
+        }else{
+            message.textContent = "Campos vacíos"
         }
     }else{
-        message.textContent = "Campos vacíos"
+        message.textContent = "El año no es válido."
     }
 })
 
